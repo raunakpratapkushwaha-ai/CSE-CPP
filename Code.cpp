d777067668d90e8ea50fdaa077258866e9abc8cf
Raunak <raunakpratapkushwaha@gmail.com>
2025-09-26 10:38:08 +0530

    learning DSA from Striver

---
+++ Code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+using namespace std;
+int main(){
+    // int a = 1;
+    // int b = 2;
+    // int c = 3;
+
+    // cout <<"sqaure of 1 is " << a * a << "\n"; 
+    // cout <<"sqaure of 2 is " << b * b << "\n";
+    // cout <<"sqaure of 3 is " << c * c << "\n";
+
+    cout << "8 + 4 is " << 8 + 4 << "\n";
+    cout << "8 - 4 is " << 8 - 4 << "\n";
+    cout << "8 * 4 is " << 8 * 4 << "\n";
+    cout << "8 / 4 is " << 8 / 4 << "\n";
+
+    cout << "7.0 / 2 is " << 7.0 / 2 << "\n";
+}
